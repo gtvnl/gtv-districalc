a2ef86f5ce37a177baa83e240224f39ce9282e33
Ramsy de Vos <ramsy@dcgs.nl>
2017-03-18 11:53:34 +0100

    .

--- app/lib/web_scraper.rb
+++ app/lib/web_scraper.rb
@@ -1,32 +1,32 @@
-require 'mechanize'
-
-class WebScraper
-  class << self
-
-    def scrape
-
-      url = "https://www.technischeunie.nl/"
-
-      request = Mechanize.new
-
-      page = request.get(url)
-      form = page.forms.last
-
-      customerNumber = form.fields[5]
-      userName = form.fields[7]
-      password = form.fields[9]
-
-      customerNumber.value = "xx"
-      userName.value = "x"
-      password.value = "xx"
-
-
-      page = form.submit
-
-      raise "not implemented"
-
-    end
-
-
-  end
-end
+# require 'mechanize'
+#
+# class WebScraper
+#   class << self
+#
+#     def scrape
+#
+#       url = "https://www.technischeunie.nl/"
+#
+#       request = Mechanize.new
+#
+#       page = request.get(url)
+#       form = page.forms.last
+#
+#       customerNumber = form.fields[5]
+#       userName = form.fields[7]
+#       password = form.fields[9]
+#
+#       customerNumber.value = "xx"
+#       userName.value = "x"
+#       password.value = "xx"
+#
+#
+#       page = form.submit
+#
+#       raise "not implemented"
+#
+#     end
+#
+#
+#   end
+# end
